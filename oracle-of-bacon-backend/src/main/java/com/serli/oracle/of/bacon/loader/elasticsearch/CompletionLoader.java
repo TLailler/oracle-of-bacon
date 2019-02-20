ec6206baadbfa8d26d1214d8a7fd7a85ab198be7
TLailler <thomas.lailler@orange.fr>
2019-02-20 15:35:04 +0100

    Première version pour l'import et la suggestion avec ElasticSearch

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
@@ -1,7 +1,12 @@
 package com.serli.oracle.of.bacon.loader.elasticsearch;
 
 import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
+
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,9 +16,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class CompletionLoader {
     private static AtomicInteger count = new AtomicInteger(0);
+    private static final long SIZE = 1048576;
+    
+    private static BulkRequest request = new BulkRequest();
+    private static RestHighLevelClient client = ElasticSearchRepository.createClient();
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        RestHighLevelClient client = ElasticSearchRepository.createClient();
 
         if (args.length != 1) {
             System.err.println("Expecting 1 arguments, actual : " + args.length);
@@ -21,13 +29,39 @@ public class CompletionLoader {
             System.exit(-1);
         }
 
+        
+        
         String inputFilePath = args[0];
         try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputFilePath))) {
             bufferedReader
                     .lines()
                     .forEach(line -> {
-                        //TODO ElasticSearch insert
-                        System.out.println(line);
+
+                    	 if(count.get() == 0) {
+                             count.getAndIncrement();
+                         } 
+                         else {
+                             String jsonString = "{ \"name\": \"" + line.replace("\"", "") + "\" }";
+                             request.add(
+                                 new IndexRequest("actors")
+                                     .id(Integer.toString(count.getAndIncrement()))
+                                     .type("_doc")
+                                     .source(jsonString, XContentType.JSON)
+                             );
+                             if(count.get() % SIZE == 0) {
+                            	 
+                             	try {
+                                     BulkResponse bulkResponse = client.bulk(request);
+                                     if(bulkResponse.hasFailures()) {
+                                         System.out.println(bulkResponse.buildFailureMessage());
+                                     }
+                                 } catch (Exception e) {
+                                     System.out.println(e.getMessage());
+                                 } finally {
+                                     request = new BulkRequest();
+                                 }
+                             }
+                         }
                     });
         }
 
@@ -35,4 +69,5 @@ public class CompletionLoader {
 
         client.close();
     }
+    	
 }
